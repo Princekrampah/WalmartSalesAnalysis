629aa9df880ee7fa616bcaae04527a38999df987
Princekrampah <jsksprince@gmail.com>
2023-05-14 18:51:40 +0300

    Documentation clean up

--- README.md
+++ README.md
@@ -24,11 +24,11 @@ The dataset was obtained from the [Kaggle Walmart Sales Forecasting Competition]
 | product_line            | Product line of the product solf        | VARCHAR(100)   |
 | unit_price              | The price of each product               | DECIMAL(10, 2) |
 | quantity                | The amount of the product sold          | INT            |
-| tax_pct                 | The amount of tax on the purchase       | FLOAT(6, 4)    |
+| VAT                 | The amount of tax on the purchase       | FLOAT(6, 4)    |
 | total                   | The total cost of the purchase          | DECIMAL(10, 2) |
 | date                    | The date on which the purchase was made | DATE           |
 | time                    | The time at which the purchase was made | TIMESTAMP      |
-| payment                 | The total amount paid                   | DECIMAL(10, 2) |
+| payment_method                 | The total amount paid                   | DECIMAL(10, 2) |
 | cogs                    | Cost Of Goods sold                      | DECIMAL(10, 2) |
 | gross_margin_percentage | Gross margin percentage                 | FLOAT(11, 9)   |
 | gross_income            | Gross Income                            | DECIMAL(10, 2) |
@@ -70,34 +70,46 @@ The dataset was obtained from the [Kaggle Walmart Sales Forecasting Competition]
 
 ## Business Questions To Answer
 
+### Generic Question
+
 1. How many unique cities does the data have?
 2. In which city is each branch?
-3. How many unique product lines does the data have?
-4. How many unique customer types does the data have?
-5. How many unique payment methods does the data have?
-6. What is the most selling product line?
-7. What is the average rating of each product line?
-8. Which time of the day do customers give most ratings?
-9. Which time of the day do customers give most ratings per branch?
-10. Which day fo the week has the best avg ratings?
-11. Which day of the week has the best average ratings per branch?
-12. Which city has the largest tax percent/ VAT (**Value Added Tax**)?
-13. What is the gender of most of the customers?
-14. What is the gender distribution per branch?
-15. Number of sales made in each time of the day per weekday
-16. What is the most common customer type?
-17. Which customer type buys the most?
-18. Which branch sold more products than average product sold?
-19. What is the most common product line by gender
-20. Fetch each product line and add a column to those product line showing "Good", "Bad". Good if its greater than average sales
-21. What is the most common payment method?
-22. What is the total revenue by month?
-23. What month had the largest COGS?
-24. What product line had the largest revenue?
-25. What is the city with the largest revenue?
-26. What product line had the largest VAT?
-27. Which of the customer types brings the most revenue?
-28. Which customer type pays the most in VAT?
+
+### Product
+
+1. How many unique product lines does the data have?
+2. What is the most common payment method?
+3. What is the most selling product line?
+4. What is the total revenue by month?
+5. What month had the largest COGS?
+6. What product line had the largest revenue?
+5. What is the city with the largest revenue?
+6. What product line had the largest VAT?
+7. Fetch each product line and add a column to those product line showing "Good", "Bad". Good if its greater than average sales
+8. Which branch sold more products than average product sold?
+9. What is the most common product line by gender?
+12. What is the average rating of each product line?
+
+### Sales
+
+1. Number of sales made in each time of the day per weekday
+2. Which of the customer types brings the most revenue?
+3. Which city has the largest tax percent/ VAT (**Value Added Tax**)?
+4. Which customer type pays the most in VAT?
+
+### Customer
+
+1. How many unique customer types does the data have?
+2. How many unique payment methods does the data have?
+3. What is the most common customer type?
+4. Which customer type buys the most?
+5. What is the gender of most of the customers?
+6. What is the gender distribution per branch?
+7. Which time of the day do customers give most ratings?
+8. Which time of the day do customers give most ratings per branch?
+9. Which day fo the week has the best avg ratings?
+10. Which day of the week has the best average ratings per branch?
+
 
 ## Revenue And Profit Calculations
 
--- SQL_queries.sql
+++ SQL_queries.sql
@@ -78,7 +78,9 @@ ALTER TABLE sales ADD COLUMN month_name VARCHAR(10);
 UPDATE sales
 SET month_name = MONTHNAME(date);
 
-
+-- --------------------------------------------------------------------
+-- ---------------------------- Generic ------------------------------
+-- --------------------------------------------------------------------
 -- How many unique cities does the data have?
 SELECT 
 	DISTINCT city
@@ -90,10 +92,126 @@ SELECT
     branch
 FROM sales;
 
+-- --------------------------------------------------------------------
+-- ---------------------------- Product -------------------------------
+-- --------------------------------------------------------------------
+
 -- How many unique product lines does the data have?
 SELECT
 	DISTINCT product_line
 FROM sales;
+
+
+-- What is the most selling product line
+SELECT
+	SUM(quantity) as qty,
+    product_line
+FROM sales
+GROUP BY product_line
+ORDER BY qty DESC;
+
+-- What is the most selling product line
+SELECT
+	SUM(quantity) as qty,
+    product_line
+FROM sales
+GROUP BY product_line
+ORDER BY qty DESC;
+
+-- What is the total revenue by month
+SELECT
+	month_name AS month,
+	SUM(total) AS total_revenue
+FROM sales
+GROUP BY month_name 
+ORDER BY total_revenue;
+
+
+-- What month had the largest COGS?
+SELECT
+	month_name AS month,
+	SUM(cogs) AS cogs
+FROM sales
+GROUP BY month_name 
+ORDER BY cogs;
+
+
+-- What product line had the largest revenue?
+SELECT
+	product_line,
+	SUM(total) as total_revenue
+FROM sales
+GROUP BY product_line
+ORDER BY total_revenue DESC;
+
+-- What is the city with the largest revenue?
+SELECT
+	branch,
+	city,
+	SUM(total) AS total_revenue
+FROM sales
+GROUP BY city, branch 
+ORDER BY total_revenue;
+
+
+-- What product line had the largest VAT?
+SELECT
+	product_line,
+	AVG(tax_pct) as avg_tax
+FROM sales
+GROUP BY product_line
+ORDER BY avg_tax DESC;
+
+
+-- Fetch each product line and add a column to those product 
+-- line showing "Good", "Bad". Good if its greater than average sales
+
+SELECT 
+	AVG(quantity) AS avg_qnty
+FROM sales;
+
+SELECT
+	product_line,
+	CASE
+		WHEN AVG(quantity) > 6 THEN "Good"
+        ELSE "Bad"
+    END AS remark
+FROM sales
+GROUP BY product_line;
+
+
+-- Which branch sold more products than average product sold?
+SELECT 
+	branch, 
+    SUM(quantity) AS qnty
+FROM sales
+GROUP BY branch
+HAVING SUM(quantity) > (SELECT AVG(quantity) FROM sales);
+
+
+-- What is the most common product line by gender
+SELECT
+	gender,
+    product_line,
+    COUNT(gender) AS total_cnt
+FROM sales
+GROUP BY gender, product_line
+ORDER BY total_cnt DESC;
+
+-- What is the average rating of each product line
+SELECT
+	ROUND(AVG(rating), 2) as avg_rating,
+    product_line
+FROM sales
+GROUP BY product_line
+ORDER BY avg_rating DESC;
+
+-- --------------------------------------------------------------------
+-- --------------------------------------------------------------------
+
+-- --------------------------------------------------------------------
+-- -------------------------- Customers -------------------------------
+-- --------------------------------------------------------------------
 
 -- How many unique customer types does the data have?
 SELECT
@@ -105,31 +223,41 @@ SELECT
 	DISTINCT payment
 FROM sales;
 
--- What is the most selling product line
+
+-- What is the most common customer type?
 SELECT
-	SUM(quantity) as qty,
-    product_line
+	customer_type,
+	count(*) as count
 FROM sales
-GROUP BY product_line
-ORDER BY qty DESC;
+GROUP BY customer_type
+ORDER BY count DESC;
+
+-- Which customer type buys the most?
+SELECT
+	customer_type,
+    COUNT(*)
+FROM sales
+GROUP BY customer_type;
 
 
--- What is the most selling product line by branch
+-- What is the gender of most of the customers?
 SELECT
-	SUM(quantity) as qty,
-    product_line
+	gender,
+	COUNT(*) as gender_cnt
 FROM sales
-WHERE branch = "A"
-GROUP BY product_line
-ORDER BY qty DESC;
+GROUP BY gender
+ORDER BY gender_cnt DESC;
 
--- What is the average rating of each product line
+-- What is the gender distribution per branch?
 SELECT
-	ROUND(AVG(rating), 2) as avg_rating,
-    product_line
+	gender,
+	COUNT(*) as gender_cnt
 FROM sales
-GROUP BY product_line
-ORDER BY avg_rating DESC;
+WHERE branch = "C"
+GROUP BY gender
+ORDER BY gender_cnt DESC;
+-- Gender per branch is more or less the same hence, I don't think has
+-- an effect of the sales per branch and other factors.
 
 -- Which time of the day do customers give most ratings?
 SELECT
@@ -164,15 +292,6 @@ ORDER BY avg_rating DESC;
 -- Mon, Tue and Friday are the top best days for good ratings
 -- why is that the case, how many sales are made on these days?
 
-SELECT 
-	day_name,
-	COUNT(day_name) total_sales
-FROM sales
-GROUP BY day_name
-ORDER BY total_sales DESC;
-
--- Monday seems to have the best avg rating, may be cause there are not
--- much sales on Monday as it rates lowest for total sales.
 
 
 -- Which day of the week has the best average ratings per branch?
@@ -182,39 +301,15 @@ SELECT
 FROM sales
 WHERE branch = "C"
 GROUP BY day_name
-ORDER BY total_sales DESC;	
+ORDER BY total_sales DESC;
 
 
--- Which city has the largest tax percent?
-SELECT
-	city,
-    ROUND(AVG(tax_pct), 2) AS avg_tax_pct
-FROM sales
-GROUP BY city 
-ORDER BY avg_tax_pct DESC;
+-- --------------------------------------------------------------------
+-- --------------------------------------------------------------------
 
-
--- What is the gender of most of the customers?
-SELECT
-	gender,
-	COUNT(*) as gender_cnt
-FROM sales
-GROUP BY gender
-ORDER BY gender_cnt DESC;
-
-
-
--- What is the gender distribution per branch?
-SELECT
-	gender,
-	COUNT(*) as gender_cnt
-FROM sales
-WHERE branch = "C"
-GROUP BY gender
-ORDER BY gender_cnt DESC;
--- Gender per branch is more or less the same hence, I don't think has
--- an effect of the sales per branch and other factors.
-
+-- --------------------------------------------------------------------
+-- ---------------------------- Sales ---------------------------------
+-- --------------------------------------------------------------------
 
 -- Number of sales made in each time of the day per weekday 
 SELECT
@@ -227,109 +322,6 @@ ORDER BY total_sales DESC;
 -- Evenings experience most sales, the stores are 
 -- filled during the evening hours
 
-
--- What is the most common customer type?
-SELECT
-	customer_type,
-	count(*) as count
-FROM sales
-GROUP BY customer_type
-ORDER BY count DESC;
-
-
--- Which customer type buys the most?
-SELECT
-	customer_type,
-    COUNT(*)
-FROM sales
-GROUP BY customer_type;
-
-
--- Which branch sold more products than average product sold?
-SELECT 
-	branch, 
-    SUM(quantity) AS qnty
-FROM sales
-GROUP BY branch
-HAVING SUM(quantity) > (SELECT AVG(quantity) FROM sales);
-
--- What is the most common product line by gender
-SELECT
-	gender,
-    product_line,
-    COUNT(gender) AS total_cnt
-FROM sales
-GROUP BY gender, product_line
-ORDER BY total_cnt DESC;
-
-
--- Fetch each product line and add a column to those product 
--- line showing "Good", "Bad". Good if its greater than average sales
-
-SELECT 
-	AVG(quantity) AS avg_qnty
-FROM sales;
-
-SELECT
-	product_line,
-	CASE
-		WHEN AVG(quantity) > 6 THEN "Good"
-        ELSE "Bad"
-    END AS remark
-FROM sales
-GROUP BY product_line;
-
--- What is the most common payment method
-SELECT
-	payment,
-    count(*) AS cnt
-FROM sales
-GROUP by payment
-ORDER BY cnt;
-
--- What is the total revenue by month
-SELECT
-	month_name AS month,
-	SUM(total) AS total_revenue
-FROM sales
-GROUP BY month_name 
-ORDER BY total_revenue;
-
--- What month had the largest COGS?
-SELECT
-	month_name AS month,
-	SUM(cogs) AS cogs
-FROM sales
-GROUP BY month_name 
-ORDER BY cogs;
-
-
--- What product line had the largest revenue?
-SELECT
-	product_line,
-	SUM(total) as total_revenue
-FROM sales
-GROUP BY product_line
-ORDER BY total_revenue DESC;
-
--- What product line had the largest VAT?
-SELECT
-	product_line,
-	AVG(tax_pct) as avg_tax
-FROM sales
-GROUP BY product_line
-ORDER BY avg_tax DESC;
-
-
--- What is the city with the largest revenue?
-SELECT
-	branch,
-	city,
-	SUM(total) AS total_revenue
-FROM sales
-GROUP BY city, branch 
-ORDER BY total_revenue;
-
 -- Which of the customer types brings the most revenue?
 SELECT
 	customer_type,
@@ -338,6 +330,14 @@ FROM sales
 GROUP BY customer_type
 ORDER BY total_revenue;
 
+-- Which city has the largest tax/VAT percent?
+SELECT
+	city,
+    ROUND(AVG(tax_pct), 2) AS avg_tax_pct
+FROM sales
+GROUP BY city 
+ORDER BY avg_tax_pct DESC;
+
 -- Which customer type pays the most in VAT?
 SELECT
 	customer_type,
@@ -345,3 +345,6 @@ SELECT
 FROM sales
 GROUP BY customer_type
 ORDER BY total_tax;
+
+-- --------------------------------------------------------------------
+-- --------------------------------------------------------------------
